912cdb64e5a1d76697e33d0da4f4572354df7484
derickcoleman87 <107764740+derickcoleman87@users.noreply.github.com>
2023-03-14 20:32:20 -0400

    working on bug in fetch.

--- app.js
+++ app.js
@@ -15,33 +15,42 @@ fetch("https://jsonplaceholder.typicode.com/posts")
     }
 
     let postTile = document.querySelectorAll(".post");
-    // let baseUrl = "https://jsonplaceholder.typicode.com/posts/";
-    // let commentUrl = "";
-    let id = data.id;
 
-    for (let i = 0; i < postTile.length; i++) {
-      postTile[i].addEventListener("click", function () {
-        console.log("click");
-        fetch(`"https://jsonplaceholder.typicode.com/posts/${id}/comments"`)
-          // fetch(baseUrl.concat(commentUrl))
-          .then((response) => response.json())
-          .then((comments) => {
-            let commentContainer = document.createElement("div");
-            commentContainer.className = ".comment-container";
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          let id = data[i].id;
+          console.log(id);
 
-            for (let i = 0; i < comments.length; i++) {
-              let comment = document.createElement("div");
-              comment.className = "comment";
-              comment.innerHTML = `
-                <h1>${comments[i].name}</h1>
-                <h2>${comments[i].email}</h2>
-                <p>${comments[i].body}</p>
-              `;
+          for (let i = 0; i < postTile.length; i++) {
+            postTile[i].addEventListener("click", function () {
+              console.log("click");
+              // fetch not working properly
+              fetch(
+                `"https://jsonplaceholder.typicode.com/posts/${id}/comments"`
+              )
+                // fetch(baseUrl.concat(commentUrl))
+                .then((response) => response.json())
+                .then((comments) => {
+                  let commentContainer = document.createElement("div");
+                  commentContainer.className = ".comment-container";
 
-              commentContainer.append(comment);
-              document.querySelector(".post").append(commentContainer);
-            }
-          });
+                  for (let i = 0; i < comments.length; i++) {
+                    let comment = document.createElement("div");
+                    comment.className = "comment";
+                    comment.innerHTML = `
+                    <h1>${comments[i].name}</h1>
+                    <h2>${comments[i].email}</h2>
+                    <p>${comments[i].body}</p>
+                  `;
+
+                    commentContainer.append(comment);
+                    document.querySelector(".post").append(commentContainer);
+                  }
+                });
+            });
+          }
+        }
       });
-    }
   });
