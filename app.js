58edb32d277f321894e5b4574a11a5ad30551450
derickcoleman87 <107764740+derickcoleman87@users.noreply.github.com>
2023-03-17 05:52:00 -0400

    working on dblclick to remove comments container.

--- app.js
+++ app.js
@@ -9,9 +9,41 @@ fetch("https://jsonplaceholder.typicode.com/posts")
         <h2>${data[i].title}</h2>
         <p>${data[i].body}</p>
       `;
-      post.addEventListener("click", function () {
-        console.log("click");
-        // fetch not working properly
+      // post.addEventListener("click", function () {
+      //   console.log("click");
+      //   // fetch not working properly
+      //   fetch(
+      //     `https://jsonplaceholder.typicode.com/posts/${data[i].id}/comments`
+      //   )
+      //     .then((response) => response.json())
+      //     .then((comments) => {
+      //       let commentContainer = document.createElement("div");
+      //       commentContainer.id = "comment-container";
+
+      //       for (let i = 0; i < comments.length; i++) {
+      //         let comment = document.createElement("div");
+      //         comment.id = "comment";
+      //         comment.innerHTML = `
+      //               <h1>${comments[i].name}</h1>
+      //               <h2>${comments[i].email}</h2>
+      //               <p>${comments[i].body}</p>
+      //             `;
+
+      //         commentContainer.append(comment);
+      //         post.append(commentContainer);
+      //       }
+      //     });
+      // });
+
+      // post.addEventListener("dblclick", function () {
+      //   console.log("dblclick");
+      //   let commentContainer = document.querySelector("#comment-container");
+      //   // let comment = document.getElementById("#comment");
+      //   // removes post not just comments.
+      //   post.remove(commentContainer);
+      // });
+
+      function singleClick() {
         fetch(
           `https://jsonplaceholder.typicode.com/posts/${data[i].id}/comments`
         )
@@ -33,15 +65,14 @@ fetch("https://jsonplaceholder.typicode.com/posts")
               post.append(commentContainer);
             }
           });
-      });
+      }
 
-      post.addEventListener("dblclick", function () {
-        console.log("dblclick");
-        let commentContainer = document.getElementById("#comment-container");
-        let comment = document.getElementsByClassName("#comment");
+      function doubleClick() {
+        let commentContainer = document.querySelector("#comment-container");
+        // let comment = document.getElementById("#comment");
         // removes post not just comments.
-        post.remove(commentContainer);
-      });
+        commentContainer.remove();
+      }
       document.querySelector(".post-container").append(post);
     }
   });
